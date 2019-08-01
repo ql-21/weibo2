e22ae3a9ea402064a96c2a498d9f3d93ff9dadc5
qulu <qlq110921@163.com>
2019-08-01 16:56:35 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    protected $fillable = ['content'];
+
     public function user()
     {
-        return $this->belongsTo(User::class);  //一条微博属于一个用户
+        return $this->belongsTo(User::class);
     }
-
 }
+
