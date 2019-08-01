1316c30a4630893357b107fa8b57eb4f5a65d87c
qulu <qlq110921@163.com>
2019-08-01 16:35:50 +0800

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -34,7 +34,10 @@ class UsersController extends Controller
     //显示用户个人信息的页面
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+                           ->orderBy('created_at', 'desc')
+                           ->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
     }
     //校验
     public function store(Request $request)
