37a4cff874cbcb49b03eec66c62e7fa690ef48c7
qulu <qlq110921@163.com>
2019-08-01 11:44:40 +0800

    授权管理员可进行删除用户操作

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -87,6 +87,7 @@ class UsersController extends Controller
     //用户删除
     public function destroy(User $user)
     {
+        $this->authorize('destroy', $user);
         $user->delete();
         session()->flash('success', '成功删除用户！');
         return back();
