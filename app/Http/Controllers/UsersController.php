1fb6403ceebd7b14c1beb85980da90152ea878fc
qulu <qlq110921@163.com>
2019-08-01 11:06:00 +0800

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,12 +10,20 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
         $this->middleware('guest', [
             'only' => ['create']
         ]);
     }
+
+    //用户列表
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
     //注册
     public function create()
     {
@@ -75,5 +83,7 @@ class UsersController extends Controller
 
         return redirect()->route('users.show', $user);
     }
+
+
 
 }
