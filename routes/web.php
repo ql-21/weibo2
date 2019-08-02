ea80a012c3b423f881423d4b414706f25944e265
qulu <qlq110921@163.com>
2019-08-02 10:14:35 +0800

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 //微博的创建和删除（RESTful）
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+//关注列表
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+//粉丝列表
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
